28c2480df81810e400641280eb30b32ad85e496d
dacodingguy <c22cg@dalton.org>
2020-10-20 17:34:20 -0400

    Add Constants as well as Target class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+class Constants {
+    public static final float TRIGGER_THRESHOLD = .65f;
+    //These need to be changed
+    public static final Target powershot1 = new Target(0,0);
+    public static final Target powershot2 = new Target(0,0);
+    public static final Target powershot3 = new Target(0,0);
+    public static final Target topGoal = new Target(0,0);
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Target.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Target {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Target(int x, int y) {
+        x = this.x;
+        y = this.y;
+    }
+}
