59ef1d40059028919b31689067fa104b980047e1
dacodingguy <c22cg@dalton.org>
2020-10-26 13:36:11 -0400

    Changed the Target constants to approximations of what they would be in a real game.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 class Constants {
     public static final float TRIGGER_THRESHOLD = .65f;
     //These need to be changed
-    public static final Target powershot1 = new Target(0,0);
-    public static final Target powershot2 = new Target(0,0);
-    public static final Target powershot3 = new Target(0,0);
-    public static final Target topGoal = new Target(0,0);
+    public static final Target powershot1 = new Target(30,32);
+    public static final Target powershot2 = new Target(36,32);
+    public static final Target powershot3 = new Target(42,32);
+    public static final Target topGoal = new Target(60,36);
 }
