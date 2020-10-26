abd4193f8f829640ccd26181188d296a34145ec0
dacodingguy <c22cg@dalton.org>
2020-10-26 13:38:31 -0400

    Added edited FTC colorsensor opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorColor.java
@@ -0,0 +1,74 @@
+/* Copyright (c) 2017-2020 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+
+@TeleOp(name = "Sensor: Color", group = "Sensor")
+public class SensorColor extends OpMode {
+
+  /** The colorSensor field will contain a reference to our color sensor hardware object */
+  NormalizedColorSensor colorSensor;
+
+  @Override public void init() {
+    //Get the sensor from the HardwareMap
+    colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+    /*Gain is the amount that all incoming values are increased with.
+    If sensor values are too low, this should be increased, and vice versa.*/
+    float gain = 2;
+    colorSensor.setGain(gain);
+
+  }
+
+  @Override
+  public void loop() {
+    final float[] hsvValues = new float[3];
+    //Get the actual colors
+    NormalizedRGBA colors = colorSensor.getNormalizedColors();
+    //Convert them into HSV values, which are more useful for computer vision tasks.
+    Color.colorToHSV(colors.toColor(), hsvValues);
+    //Add all the values to telemetry
+    telemetry.addLine()
+            .addData("Red", "%.3f", colors.red)
+            .addData("Green", "%.3f", colors.green)
+            .addData("Blue", "%.3f", colors.blue);
+    telemetry.addLine()
+            .addData("Hue", "%.3f", hsvValues[0])
+            .addData("Saturation", "%.3f", hsvValues[1])
+            .addData("Value", "%.3f", hsvValues[2]);
+    telemetry.addData("Alpha", "%.3f", colors.alpha);
+    telemetry.update();
+  }
+}
