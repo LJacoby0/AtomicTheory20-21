f57d892fa7763d2823c2110b8701cadb13266f72
charlie.garfield <c22cg@dalton.org>
2020-12-03 19:09:17 -0500

    Added RoadRunnerPark autonomous as a test and template.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerPark.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "Park", group = "Remote")
+public class RoadRunnerPark extends LinearOpMode {
+    Robot rb = new Robot(telemetry);
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startingPose = new Pose2d(-62, 50, Math.toRadians(90));
+        Trajectory newTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .splineTo(new Vector2d(6, 50), Math.toRadians(90))
+                .build();
+        waitForStart();
+        drive.followTrajectory(newTrajectory);
+    }
+
+}
